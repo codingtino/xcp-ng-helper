523ba18d34ce25a95e5fe8ec1b03ea8baa2502ba
t.naumann <t.naumann@gge-germany.de>
2024-10-31 08:22:19 +0100

    first commit

---
+++ create_vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###########################################################
+# Title: XCP-ng - Helper | create VM                      #
+# Author: Tino Naumann                                    #
+# Repository: https://github.com/codingtino/xcp-ng-helper #
+###########################################################
+
+
